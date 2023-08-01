f17b9605b9ad39ccd2131de91601442f44d34083
Faisal Kabir <onlyfaisalkabir@gmail.com>
2023-08-01 17:13:08 +0600

    new subject adding modified

--- elements/addnewsubject.js
+++ elements/addnewsubject.js
@@ -6,6 +6,7 @@ class AddNewSub {
         this.tutorAccountLink = Selector('.sidebar-list a.sidebar-link.sidebar-title').withText('Tutor Account');
         this.addTuitionButton = Selector('.u-pearl-icon i.icofont.icofont-notebook').parent();
         this.addNew = Selector('#add-new');
+        this.subjectCategory = Selector('.css-ackcql');
     }
 
     async clickTutorAccountLink() {
@@ -17,8 +18,18 @@ class AddNewSub {
         await t.click(this.addTuitionButton);
         await t.wait(2000);
     }
-    async addNewSubject(){
+    async addNewSubject() {
         await t.click(this.addNew);
+        await t.click(this.subjectCategory);
+        const options = ['ACADEMIC', 'ANTIQUES & COLLECTIBLES', 'ARCHITECTURE', 'ART', 'ARTS'];
+
+        // Get a random index to select one of the options randomly.
+        const randomIndex = Math.floor(Math.random() * options.length);
+
+        const optionSelector = Selector(`input[id^="react-select-"]`).withAttribute('value', options[randomIndex]);
+
+        await t.click(optionSelector);
+
     }
 
     async performActions() {
