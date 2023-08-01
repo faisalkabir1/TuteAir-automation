3840989898c87192a8c5b31806ed78fb4ddea081
Faisal Kabir <onlyfaisalkabir@gmail.com>
2023-08-01 16:13:25 +0600

    Update addnewsubject.js

--- elements/addnewsubject.js
+++ elements/addnewsubject.js
@@ -5,6 +5,7 @@ class AddNewSub {
 
         this.tutorAccountLink = Selector('.sidebar-list a.sidebar-link.sidebar-title').withText('Tutor Account');
         this.addTuitionButton = Selector('.u-pearl-icon i.icofont.icofont-notebook').parent();
+        this.addNew = Selector('#add-new');
     }
 
     async clickTutorAccountLink() {
@@ -16,10 +17,14 @@ class AddNewSub {
         await t.click(this.addTuitionButton);
         await t.wait(2000);
     }
+    async addNewSubject(){
+        await t.click(this.addNew);
+    }
 
     async performActions() {
         await this.clickTutorAccountLink();
         await this.clickAddTuitionButton();
+        await this.addNewSubject();
     }
 }
 
